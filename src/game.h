b416ce2772129bd2892a1a5250f264a8e03ada93
Vũ Minh Dũng <dung.vu@finhay.com.vn>
2022-11-21 12:01:33 +0700

    update cmake

--- src/game.h
+++ src/game.h
@@ -7,9 +7,9 @@
 
 #include <stdio.h>
 #include "SDL2/SDL.h"
-#include "SDL2/SDL_image.h"
-#include "SDL2/SDL_mixer.h"
-#include "SDL2/SDL_ttf.h"
+#include "SDL2_image/SDL_image.h"
+#include "SDL2_mixer/SDL_mixer.h"
+#include "SDL2_ttf/SDL_ttf.h"
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdbool.h>
